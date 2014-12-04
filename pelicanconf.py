6a9e92b5d4d08437b447dc90bcd373714bb61aa1
Kyle Barlow <kb@kylebarlow.com>
2014-12-03 21:48:26 -0800

    Got search working

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,6 +36,15 @@ THEME = 'pelican-elegant'
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
+PLUGIN_PATHS = ['pelican-plugins/']
+PLUGINS = ['sitemap', 'extract_toc', 'tipue_search']
+MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
+DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
+STATIC_PATHS = ['theme/images', 'images']
+TAG_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+
 # Home page
 LANDING_PAGE_ABOUT = {
     'title' :'This is my landing page about',
