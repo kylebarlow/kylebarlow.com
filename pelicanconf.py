58adeec65db6547f714b624754325b904a7b1f89
Kyle Barlow <kb@kylebarlow.com>
2014-12-04 12:09:04 -0800

    Added some static files

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+import os
+
 AUTHOR = u'Kyle Barlow'
 SITENAME = u'Kyle Barlow'
 SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'America/Los_Angeles'
 
 DEFAULT_LANG = u'en'
 
@@ -20,14 +22,17 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Beaker Report', 'http://www.beakerreport.com'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('Twitter', 'https://twitter.com/kbsci'),
+    ('Facebook', 'https://www.facebook.com/kyleabarlow'),
+    ('Google-Plus', 'https://plus.google.com/+KyleBarlow'),
+    ('Github', 'https://github.com/kylebarlow'),
+)
 
 DEFAULT_PAGINATION = 10
 
@@ -40,7 +45,27 @@ PLUGIN_PATHS = ['pelican-plugins/']
 PLUGINS = ['sitemap', 'extract_toc', 'tipue_search', 'filetime_from_git']
 MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
 DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
-STATIC_PATHS = ['theme/images', 'images']
+IGNORE_FILES = ['.#*']
+
+STATIC_PATHS = [
+    'theme/images', 'images',
+    'bsg', 'crickettherm', 'android',
+]
+
+ARTICLE_EXCLUDES = [
+    'bsg', 'crickettherm', 'android',
+]
+
+EXTRA_PATH_METADATA = {}
+
+static_dir = 'static_root'
+# All files in static will be placed at root level (must be files)
+for f in os.listdir( os.path.join(PATH, static_dir) ):
+    fpath = os.path.join(static_dir, f)
+    STATIC_PATHS.append(fpath)
+    EXTRA_PATH_METADATA[fpath] = { 'path' : f }
+    ARTICLE_EXCLUDES.append(fpath)
+
 TAG_SAVE_AS = ''
 CATEGORY_SAVE_AS = ''
 AUTHOR_SAVE_AS = ''
@@ -48,7 +73,7 @@ AUTHOR_SAVE_AS = ''
 # Home page
 LANDING_PAGE_ABOUT = {
     'title' :'This is my landing page about',
-    'details' : 'Here are the details',
+    'details' : '<p>My name is Kyle Barlow and I am a computational biologist living in the beautiful San Francisco bay area (not pictured, so take my word for it). <A HREF="http://calband.berkeley.edu/">Go bears!</A></p><p>You can contact me at <A HREF="mailto:kb@kylebarlow.com">kb@kylebarlow.com</A> (my GPG public key is <A HREF="KyleBarlow_public.asc">here)</A> or find me at <A HREF="http://www.linkedin.com/in/kyleabarlow">LinkedIn</A> or on <a href="http://plus.google.com/106160593415513011050?prsrc=3" rel="publisher">Google+</A>.</p><p>Check out my <A HREF="https://github.com/kylebarlow">GitHub profile</A> for the source code for <A HREF="https://play.google.com/store/apps/details?id=com.kylebarlow.android.crickettherm">CricketTherm</A> and my other open source projects.</p>',
 }
 
 PROJECTS = [
