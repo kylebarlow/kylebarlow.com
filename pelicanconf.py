ca03850a7a19a5acecee62cadfd5dcb5734325d9
Kyle Barlow <kb@kylebarlow.com>
2015-04-09 17:33:20 -0700

    Adding license

--- pelicanconf.py
+++ pelicanconf.py
@@ -113,3 +113,5 @@ PROJECTS = [
     'description': 'Android app to estimate outdoors with no network connection based on cricket chirping rate'
 },
 ]
+
+SITE_LICENSE = u'<a href="http://www.kylebarlow.com">kylebarlow.com</a> by Kyle Barlow is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a>'
