97224f4e29cab5a5bd22baaa029c9f81f3930b39
Kyle Barlow <kb@kylebarlow.com>
2016-04-06 15:11:32 -0700

    Adding publications section to "Science" page

--- content/pages/science.rst
+++ content/pages/science.rst
@@ -1,6 +1,14 @@
 Science
 #################
 
-Poster presentations:
+Publications
+------------
+
+.. raw:: html
+
+	 <script src="http://bibbase.org/show?bib=https%3A%2F%2Fapi.zotero.org%2Fusers%2F904733%2Fcollections%2FRVQTQ8ZI%2Fitems%3Fkey%3DDjxzePBoNGaDvO4Yww8LhB9h%26format%3Dbibtex&jsonp=1&jsonp=1&css=http://bibbase.org/css/styles/default.css&jsonp=1"></script>
+
+Poster presentations
+--------------------
 
 `UCSF Discovery Fellows Symposium 2015 <{filename}/files/KAB-2015-discovery-fellows-poster.pdf>`_
--- pelicanconf.py
+++ pelicanconf.py
@@ -92,7 +92,7 @@ AUTHOR_SAVE_AS = ''
 # Home page
 LANDING_PAGE_ABOUT = {
     'title' : "Kyle Barlow",
-    'details' : '<p>I am a computational biologist PhD student living in the San Francisco bay area. <A HREF="http://calband.berkeley.edu/">Go bears!</A></p><p>You can contact me using my <A HREF="KyleBarlow_public.asc">GPG public key.</A></p><p>Check out my <A HREF="https://github.com/kylebarlow">GitHub profile</A> for the source code for <A HREF="https://play.google.com/store/apps/details?id=com.kylebarlow.android.crickettherm">CricketTherm</A> and my other open source projects.</p>',
+    'details' : '<p>I am a computational biologist PhD student living in the San Francisco bay area. <A HREF="http://calband.berkeley.edu/">Go bears!</A></p><p>You can contact me securely by using my <A HREF="KyleBarlow_public.asc">GPG public key</A> or via <A HREF="https://keybase.io/kylebarlow">Keybase</A></p><p>Check out my <A HREF="https://github.com/kylebarlow">GitHub profile</A> for the source code for <A HREF="https://play.google.com/store/apps/details?id=com.kylebarlow.android.crickettherm">CricketTherm</A> and my other open source projects.</p>',
 }
 
 PROJECTS = [
